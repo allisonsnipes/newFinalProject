c0f807a2d27b487d13b602dd35e466f0900797ed
Allison Snipes <ajsnipes@usfca.edu>
2019-07-20 09:12:21 -0700

    game and home done

--- project/src/components/jumbotron/index.js
+++ project/src/components/jumbotron/index.js
@@ -1,12 +1,15 @@
-import React from 'react'
+import React from 'react';
 
-function Jumbotron () {
-  return (
-    <div class='jumbotron'>
-      <h1> Welcome to Game Meet</h1>
-      <p> Please select the choice of loggin in or creating an acoount. After you have completed these actions you will be shown a listing of meetup groups for games in a specific area.</p>
-    </div>
-  )
+function Jumbotron() {
+	return (
+		<div className="jumbotron">
+			<h1> Welcome to Game Meet!</h1>
+			<p>
+				Please select the choice of either logging in or creating an acoount. After you have completed these
+				actions you will be shown listings of meetup groups for games in a specific area.
+			</p>
+		</div>
+	);
 }
 
-export default Jumbotron
+export default Jumbotron;
