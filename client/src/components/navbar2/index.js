362bfd3adb3ae0bda774e438c01b1b02229340aa
Allison Snipes <ajsnipes@usfca.edu>
2019-08-01 19:18:44 -0700

    nav is done

--- client/src/components/navbar2/index.js
+++ client/src/components/navbar2/index.js
@@ -1,24 +1,17 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import ModalCreateEvent from "../modals/modalCreateEvent";
+import React from 'react'
+import { Link } from 'react-router-dom'
+import ModalCreateEvent from '../modals/modalCreateEvent'
 
-function NavbarII() {
+function NavbarToo () {
   return (
     // creating the second navbar with home button, and creating event
-    <nav className="navbar sticky-top navbar-dark bg-dark">
-      <Link className="nav-item text-white" to="home">
-        {" "}
-        Home{" "}
-      </Link>
-      <Link className="nav-item text-white" to="games">
-        {" "}
-        Games{" "}
-      </Link>
-      <Link className="nav-item text-white" to="/">
-        {" "}
-        <h1>Game Meet! </h1>{" "}
-      </Link>
-      <ModalCreateAccount />
+    <nav className='navbar sticky-top navbar-dark bg-dark'>
+      <Link className='nav-item text-white' to='home'>Home </Link>
+      <Link className='nav-item text-white' to='games'>Games </Link>
+      <Link className='nav-item text-white' to='/'><h1>Game Meet! </h1></Link>
+      <ModalCreateEvent />
     </nav>
-  );
+  )
 }
+
+export default NavbarToo
